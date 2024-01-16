2add48bb4185b46f59ddb2e95318b300d1083dcc
ariellepollock <ariellecpollock@gmail.com>
2024-01-16 17:05:54 -0600

    initial commit

---
+++ py-pac-poe.py
@@ -0,0 +1,16 @@
+##############################################
+#  STEP 1 - display welcome message
+##############################################
+def display_message():
+    message = "Let's play Py-Pac-Poe!"
+    frame = "-" * len(message)
+    print(frame)
+    print(message)
+    print(frame)
+
+display_message()
+
+##############################################
+#  STEP 2 - see printed board + existing moves
+##############################################
+
